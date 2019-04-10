602a141b56bb7b0f6a9e3dab6443e658cb4a4eaa
ayunas <ayunas@gmail.com>
2019-04-10 15:20:25 -0500

    set up project locally, created a react-app friends, started the local API endpoint. and rendered some basic text

---
+++ friends/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Testing my Create React APP friends
+        </header>
+          
+      </div>
+    );
+  }
+}
+
+export default App;
