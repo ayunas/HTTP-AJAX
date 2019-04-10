b0df15216c08412c0fcea559dcdcaf6dd265dbe0
ayunas <ayunas@gmail.com>
2019-04-10 15:24:04 -0500

    added state to constructor() and componentDidMount() and setState() to test out my <App>

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data : []
+    }
+
+  }
+
+
+  componentDidMount() {
+
+    this.setState({
+      data : ['test','test1','test2']
+    })
+
+  }
+
+
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
           Testing my Create React APP friends
         </header>
-          
       </div>
     );
   }
