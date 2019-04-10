763f519c0b488166c39b0d76785f3e49459c51bc
ayunas <ayunas@gmail.com>
2019-04-10 15:50:46 -0500

    created a <Friend> and <Route>d to it in <App>.  <Friend> displays text that is rendered to the screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom';
+import Friend from './Components/Friend';
 
 class App extends Component {
   constructor() {
@@ -26,14 +27,13 @@ class App extends Component {
       })
   }
 
-
   render() {
-    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
-          Testing my Create React APP friends
+          These are my friends
         </header>
+        <Route path='/' component={Friend}/>
       </div>
     );
   }
---
+++ friends/src/Components/Friend.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Friend = () => {
+    return <div>I'm a friend</div>
+}
+
+export default Friend;
